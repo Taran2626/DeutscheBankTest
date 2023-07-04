a4b63e4737fe31fde71b17f1d5bff225f728f446
Taranjeet Kaur <taran02611@gmail.com>
2023-07-04 15:12:12 +0530

    Initial Commit

---
+++ DeutscheBankTest/DeutscheBankTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeutscheBankTestApp.swift
+//  DeutscheBankTest
+//
+//  Created by Taranjeet Kaur on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DeutscheBankTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
